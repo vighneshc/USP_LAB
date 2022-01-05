17fe30f417194040d0c234a283e2cfd5c6d08544
vighneshc <71492836+vighneshc@users.noreply.github.com>
2022-01-05 21:02:45 +0530

    Update lab13.sh

--- lab13.sh
+++ lab13.sh
@@ -1,56 +1,39 @@
-echo "Student Mark List"
-echo Enter the Student name 
-read name 
-echo Enter the Register number 
-read rno 
-echo Enter the Mark1 
-read m1 
-echo Enter the Mark2 
-read m2 
-echo Enter the Mark3 
-read m3 
-echo Enter the Mark4 
-read m4 
-echo Enter the Mark5 
-read m5 
-tot=`expr $m1 + $m2 + $m3 + $m4 + $m5` 
-avg=`expr $tot / 5`
-echo ----------------------------------- 
-echo "Student Mark List" 
-echo ----------------------------------- 
-echo "Student Name    : $name" 
-echo "Register Number : $rno" 
-echo "Mark1           : $m1" 
-echo "Mark2           : $m2" 
-echo "Mark3           : $m3" 
-echo "Mark4           : $m4" 
-echo "Mark5           : $m5" 
-echo "Total           : $tot" 
-echo "Average         : $avg" 
-if [ $m1 -ge 35 ] && [ $m2 -ge 35 ] && [ $m3 -ge 35 ] && [ $m4 -ge 35 ] && [ $m5 -ge 35 ] 
+#!/bin/bash
+
+echo "Enter the number of subjects"
+read n
+while [ $n -gt 0 ]
+do
+echo "Enter the CIE Marks"
+read cie
+echo "Enter the SEE Marks"
+read see
+see=`expr $see / 2`
+if [ `expr $see % 2` -nt 0 ]
+then
+see=`expr see + 1`
+fi
+sum=`expr $cie + $see`
+if [ $sum -gt 89 ]
 then 
- echo "Result          : Pass" 
-  
- if [ $avg -ge 90 ] 
- then 
-  echo "Grade           : S" 
- elif [ $avg -ge 80 ] 
- then 
-         echo "Grade           : A" 
- elif [ $avg -ge 70 ] 
- then 
-         echo "Grade           : B" 
- elif [ $avg -ge 60 ] 
- then 
-         echo "Grade           : C" 
- elif [ $avg -ge 50 ] 
- then 
-         echo "Grade           : D" 
- elif [ $avg -ge 35 ] 
- then 
-         echo "Grade           : E" 
- fi 
-else 
- echo "Result          : Fail" 
-fi 
-echo ----------------------------------- 
+echo "The Grade is : S"
+elif [ $sum -gt 79 -a $sum -lt 90 -a $see -ge 20 ]
+then 
+echo "The Grade is : A"
+elif [ $sum -gt 69 -a $sum -lt 80 -a $see -ge 20 ]
+then 
+echo "The Grade is : B"
+elif [ $sum -gt 59 -a $sum -lt 70 -a $see -ge 20 ]
+then 
+echo "The Grade is : C"
+elif [ $sum -gt 49 -a $sum -lt 60 -a $see -ge 20 ]
+then 
+echo "The Grade is : D"
+elif [ $sum -gt 39 -a $sum -lt 50 -a $see -ge 20 ]
+then 
+echo "The Grade is : E"
+else
+echo "The Grade is : F"
+fi
+n=`expr $n - 1`
+done
